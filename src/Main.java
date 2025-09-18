ba55d37cfae66cae8d22c6a6945488869a2ea992
enoshindupalli <indupallienosh2023@gmail.com>
2025-09-18 14:44:54 -0500

    This is inheritance demo project

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("Generic Animal", "Huge",400);
+        doAnimalStuff(animal, "slow");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
+
+        Dog yorkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yorkie,"fast");
+
+        Dog retriever = new Dog("Lab Retriever", 65,"Floppy", "Swimmer");
+        doAnimalStuff(retriever,"slow");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
+    }
+}
